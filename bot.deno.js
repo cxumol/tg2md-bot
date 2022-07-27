7c460aa0fd0464b04c92b015cdb5ea34800e8a33
cxumol <cxumol@users.noreply.github.com>
2022-07-26 22:38:48 -0700

    Create bot.deno.js

---
+++ bot.deno.js
@@ -0,0 +1,126 @@
+import {
+    Bot,
+    webhookCallback
+} from "https://deno.land/x/grammy@v1.9.1/mod.ts"
+import { serve } from "https://deno.land/x/sift@0.5.0/mod.ts";
+
+const bot = new Bot(Deno.env.get("BOTTOKEN") || "");
+
+bot.command("start", (ctx) => ctx.reply("Welcome\\! You can send me *_RiCh TExt_* `now` \\.", { parse_mode: "MarkdownV2" })); 
+
+bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+
+bot.command("help", (ctx) => ctx.reply(`Send me *rich* _text_ and I will send it back in Markdown\\.
+
+By default, ||spoiler|| and __underline__ are ignored, because these styles are not available in most markdown standards\\.
+
+To force convert "spoiler" and "underline" into a HTML style, add \\!unstable at the beginning of your rich text\\.`, { parse_mode: "MarkdownV2" }));
+
+bot.command("source", (ctx) => ctx.reply("Welcome! Up and running.")); 
+
+const entitiesWrap = {
+  "bold": "\*\*", // TG MarkdownV2  use single \*
+  "italic": "_",
+  //"underline": "__", // (most of) markdown does not support underline
+  "code": "`",
+  // "pre": "\n```\n",
+  "strikethrough": "~~" // TG MarkdownV2  use single \~
+  // "text_link" : "[]()"
+  // "url": undefined,
+}
+//   
+const entitiesBeforeAfter = {
+  "pre": {'before':'```\n', 'after':'\n```'},
+}
+const entitiesBeforeAfterHTML = {
+  "underline": {'before':'<u>', 'after':'</u>'},
+  "spoiler": {'before':'<span style="color:black;background:black;">', 'after':'</span>'},
+}
+
+function addInsertion(idx, sym, insertions, fn){
+  if (idx in insertions){
+    if (fn==="unshift"){
+      insertions[idx].beginning.unshift(sym)
+    }else if(fn==="push"){
+      insertions[idx].ending.push(sym)
+    }
+  }else{
+    if (fn==="unshift"){
+      insertions[idx]={"beginning":[sym,],
+                          "ending":[""]}
+    }else if(fn==="push"){
+      insertions[idx]={"beginning":[""],
+                          "ending":[sym,]}
+    }
+  }
+}
+  
+bot.on("message:entities").filter(
+  (ctx) => ctx.senderChat === undefined,
+ (ctx) => {
+   try {
+            /*
+            ctx.reply(`${ctx.msg.text}
+       ${JSON.stringify(ctx.msg.entities)}`)
+       */
+
+    const entities = ctx.msg.entities
+    const txt = ctx.msg.text //.replace('\n', '  \n')
+    const txtlength = ctx.msg.text.length
+    var insertions = {"0":{"beginning":[""],
+                          "ending":[""]}
+                      };
+    insertions[ctx.msg.text.length]={"beginning":[""],
+                          "ending":[""]}; 
+
+    entities.sort((a,b)=>a.offset - b.offset || a.length-b.length//b["length"] - a["length"]
+     )
+    for ( let ett of entities){
+      if (ett.type in entitiesWrap){ 
+        addInsertion( ett.offset, entitiesWrap[ett.type], insertions, "unshift"); // 本来已经是数字类型, 不用转换
+        addInsertion( ett.offset+ett.length, entitiesWrap[ett.type], insertions, "push");
+      }else if (ett.type in entitiesBeforeAfter){
+        addInsertion( ett.offset, entitiesBeforeAfter[ett.type].before, insertions, "unshift");
+        addInsertion( ett.offset+ett.length, entitiesBeforeAfter[ett.type].after, insertions, "push");
+      }else if (txt.startsWith('!unstable') && ett.type in entitiesBeforeAfterHTML){
+        addInsertion( ett.offset, entitiesBeforeAfterHTML[ett.type].before, insertions, "unshift");
+        addInsertion( ett.offset+ett.length, entitiesBeforeAfterHTML[ett.type].after, insertions, "push");
+      }else if (ett.type=="text_link"){
+        addInsertion( ett.offset, "[", insertions, "unshift");
+        addInsertion( ett.offset+ett.length, `](${ett.url})`, insertions, "push");
+      }
+    }
+
+    
+    let textArr = [];
+    const sliceAts =  Object.keys(insertions).sort((a,b)=>Number(a)-Number(b))
+    textArr.push(insertions[sliceAts[0]].ending.join("") + insertions[sliceAts[0]].beginning.join(""))
+    for (let i=0; i<sliceAts.length-1;i++){
+      textArr.push( ctx.msg.text.slice( Number(sliceAts[i]), Number(sliceAts[i+1]) ) + insertions[sliceAts[i+1]].ending.join("") + insertions[sliceAts[i+1]].beginning.join("") )
+    }
+
+      ctx.reply( textArr.join("") ) // + "\n\n---DEBUG---\n" + JSON.stringify(insertions)
+      //  console.log(ctx.msg)
+  } catch (err) {
+    console.error(err, insertions, ctx.msg.entities);
+  }
+ }
+ );
+ 
+const handleUpdate = webhookCallback(bot, "std/http");
+
+serve({
+  ["/" + Deno.env.get("BOTTOKEN")]: async (req) => {
+    if (req.method == "POST") {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    return new Response();
+  },
+  "/": () => {
+    return new Response("Hello world!");
+  },
+});
